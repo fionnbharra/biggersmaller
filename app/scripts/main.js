3bc362274aad076afb834958d76d757485a46e11
finbar sheahan <finbar.sheahan@wearefriday.com>
2014-02-28 14:03:44 +0000

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global biggersmaller, $*/
+
+
+window.biggersmaller = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    biggersmaller.init();
+});
